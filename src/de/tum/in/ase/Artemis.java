e25b4b85a7c080ac194d218f9b3178c6742f301f
MetehanOner <a.metehanoner@gmail.com>
2022-12-19 16:32:48 +0100

    Second commit

--- src/de/tum/in/ase/Artemis.java
+++ src/de/tum/in/ase/Artemis.java
@@ -9,9 +9,9 @@ import java.util.stream.Stream;
 public class Artemis {
     // TODO: calculate the average grade of all valid exams
     public static double averageGrade(Stream<Exam> exams) {
-        double sumOfNumbers = exams.filter(e -> e.getGrade().getStatus().equals(Status.VALID)).count();
+        double sumOfNumbers = exams.parallel().filter(e -> e.getGrade().getStatus().equals(Status.VALID)).count();
 
-        double sumOfGrades = exams.filter(e -> e.getGrade().getStatus().equals(Status.VALID)).map(exam -> exam.getGrade().getValue()).reduce(0.0, Double::sum);
+        double sumOfGrades = exams.parallel().filter(e -> e.getGrade().getStatus().equals(Status.VALID)).map(exam -> exam.getGrade().getValue()).reduce(0.0, Double::sum);
 
         return sumOfGrades/sumOfNumbers;
     }
