c323f7eb7b78c23089e04ec29a9f935640d1b6d5
MetehanOner <a.metehanoner@gmail.com>
2022-12-20 09:48:11 +0100

    zweitte tür changes

--- src/de/tum/in/ase/Artemis.java
+++ src/de/tum/in/ase/Artemis.java
@@ -2,6 +2,7 @@ package de.tum.in.ase;
 
 import java.time.LocalDate;
 import java.util.*;
+import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -35,14 +36,19 @@ public class Artemis {
     // TODO: count how many exams are online and onsite
     public static Map<Boolean, Integer> countByOnline(Stream<Exam> exams) {
 
-        return null;
+        Map<Boolean, Integer> m = exams
+                .filter(e -> e.getGrade().getStatus().equals(Status.VALID))
+                .collect(Collectors.toMap(Exam::isOnline, s -> 1, Integer::sum));
+
+        return m;
     }
 
 
 	// TODO: create a report using the passed formatter
-	//public static String createFormattedReport(Stream<Exam> exams, Formatter formatter) {
-	//	return null;
-	//}
+	public static String createFormattedReport(Stream<Exam> exams, Formatter formatter) {
+
+        return null;
+	}
 
     // TODO: create a simple report string
     public static String createSimpleReport(Stream<Exam> exams) {
